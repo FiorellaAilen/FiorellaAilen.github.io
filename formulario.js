1328e21df8a407b043b68e49e69c74f4f61efc97
FiorellaAilen <fiorellaailen@gmail.com>
2023-10-11 17:32:14 -0300

    inicio form js

---
+++ formulario.js
@@ -0,0 +1,18 @@
+const enviar = document.getElementById("enviar");
+/*
+const cumple = document.getElementById("birthday");
+const correo = document.getElementById("email");
+const comen = document.getElementById("subject");
+*/
+
+function form() {
+    let name = document.getElementById("name");
+    let nombre = name.ariaValueMax.trim();
+    let birthday = document.getElementById("birthday");
+    let cumple = birthday.ariaValueMax.trim();
+    let email = document.getElementById("email");
+    let correo = email.ariaValueMax.trim();
+    let terms = document.getElementById("terms");
+    let term = terms.checked;
+
+}
