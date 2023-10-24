d958ad2b4a76618a139ece92f1e008126418ca6e
FiorellaAilen <fiorellaailen@gmail.com>
2023-10-24 15:25:51 -0300

    form

--- formulario.js
+++ formulario.js
@@ -2,14 +2,18 @@
 function validar() {
 
     //Obtener los datos ingresados
-    let name = document.getElementById("name");
-    let nombre = name.value.trim();
+    let nombre = document.getElementById("nombre");
+    let nomb = nombre.value.trim();
+    let apellido = document.getElementById("apellido");
+    let surn = apellido.value.trim();
     // let birthday = document.getElementById("birthday");
     // let cumple = birthday.value.trim();
-    let age = document.getElementById("age");
-    let edad = age.value.trim();
+    let edad = document.getElementById("edad");
+    let ed = edad.value.trim();
     let email = document.getElementById("email");
     let correo = email.value.trim();
+    let comentario = document.getElementById("comentario");
+    let comen = comentario.value.trim();
     let terms = document.getElementById("terms");
     let term = terms.checked;
     let patron = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -17,26 +21,35 @@ function validar() {
     //Validar los campos
     let valid = true;
 
-    if (nombre === "") {
+    if (nomb === "") {
         document.querySelector('#errorname').style.display = 'inline';
-        name.focus();
-        invalid = false;
+        nombre.focus();
+        valid = false;
     } else {
         document.querySelector('#errorname').style.display = 'none';
     }
+
+    if (surn === "") {
+        document.querySelector('#errorsurn').style.display = 'inline';
+        apellido.focus();
+        valid = false;
+    } else {
+        document.querySelector('#errorsurn').style.display = 'none';
+    }
+
     /*
         if (cumple === "") {
             document.querySelector('#errorbirth').style.display = 'inline';
             birthday.focus();
-            invalid = false;
+            valid = false;
         } else {
             document.querySelector('#errorage').style.display = 'none';
         }*/
 
-    if (edad === "") {
+    if (ed === "") {
         document.querySelector('#errorage').style.display = 'inline';
-        age.focus();
-        invalid = false;
+        edad.focus();
+        valid = false;
     } else {
         document.querySelector('#errorage').style.display = 'none';
     }
@@ -44,27 +57,35 @@ function validar() {
     if (!patron.test(correo)) {
         document.querySelector('#erroremail').style.display = 'inline';
         email.focus();
-        invalid = false;
+        valid = false;
     } else {
         document.querySelector('#erroremail').style.display = 'none';
+    }
+
+    if (comen === "") {
+        document.querySelector('#errorcom').style.display = 'inline';
+        comentario.focus();
+        valid = false;
+    } else {
+        document.querySelector('#errorcom').style.display = 'none';
     }
 
     if (!term) {
         document.querySelector('#errorcheck').style.display = 'inline';
         terms.focus();
-        invalid = false;
+        valid = false;
     } else {
         document.querySelector('#errorcheck').style.display = 'none';
     }
 
-    if (invalid) {
+    if (valid) {
         document.getElementById("form-content").style.display = "none";
         document.getElementById("confirm").style.display = "block";
 
         // Muestra los datos ingresados
         document.getElementById("datos-ingresados").style.display = "block";
         document.getElementById("nombre-ingresado").textContent = nombre;
-        document.getElementById("cumple-ingresado").textContent = cumple;
+        // document.getElementById("cumple-ingresado").textContent = cumple;
         document.getElementById("edad-ingresada").textContent = edad;
         document.getElementById("email-ingresado").textContent = correo;
         return false;
